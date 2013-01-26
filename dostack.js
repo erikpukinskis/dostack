5cd441e1b28667ca9d0c007a345c708cbaab18b0
Erik Pukinskis <erik@snowedin.net>
2013-01-25 19:49:45 -0800

    Items can be added and are persisted in localstorage

--- dostack.js
+++ dostack.js
@@ -1,16 +1,35 @@
 doStackApp = angular.module('doStackApp', ['ngResource']);
 
-doStackApp.controller('AppCtrl', function($scope) {
-  var items = [
-    {text: "hi", done: false},
-    {text: "ho", done: false}
-  ];
+doStackApp.controller('AppCtrl', function AppCtrl($scope, $location, doStackStorage, filterFilter) {
+  var items = $scope.items = doStackStorage.get();
 
-  $scope.items = items;
+  $scope.$watch('items', function() {
+    doStackStorage.put(items);
+  }, true);
 
   $scope.remaining = function() {
     return _.reduce(items, function(count, item) {
       return count + (item.done ? 0 : 1);
     }, 0);
   }
+
+  $scope.add = function(newItem) {
+    var item = {text: newItem.text, done: false};
+    items.push(item);
+    newItem.text = '';
+  };
+});
+
+doStackApp.factory( 'doStackStorage', function() {
+  var STORAGE_ID = 'dostack';
+
+  return {
+    get: function() {
+      return JSON.parse(localStorage.getItem(STORAGE_ID) || '[]');
+    },
+
+    put: function( items ) {
+      localStorage.setItem(STORAGE_ID, JSON.stringify(items));
+    }
+  };
 });
